f691e8dd4dc4631b5c8c932bf2e7a1c7e44fcdda
jinxiaobo <jinxiaobo@ebaolife.net>
2019-10-23 17:28:55 +0800

    init

---
+++ src/main/java/com/neo/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.neo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
